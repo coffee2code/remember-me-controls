920c22db70eb5ee1e6ce88ab0e5e2e789e593e99
Scott Reilly <scott@coffee2code.com>
2019-06-28 22:57:51 -0600

    Update License URI to be HTTPS

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -21,6 +21,7 @@
 * Change: Note compatibility through WP 5.2+
 * Change: Add link to plugin's page in Plugin Directory to README.md
 * Change: Update copyright date (2019)
+* Change: Update License URI to be HTTPS
 * Change: Split paragraph in README.md's "Support" section into two
 
 
--- remember-me-controls.php
+++ remember-me-controls.php
@@ -7,7 +7,7 @@
  * Author URI:  http://coffee2code.com/
  * Text Domain: remember-me-controls
  * License:     GPLv2 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Description: Have "Remember Me" checked by default on the login page and configure how long a login is remembered. Or disable the feature altogether.
  *
  * Compatible with WordPress 4.7+ through 5.2+.
